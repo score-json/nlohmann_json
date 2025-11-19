14e403a860cc6f8c49a62537fa11ba5fe6dae696
Erik Hu <erik.hu@d-fine.com>
2025-11-19 14:45:36 +0100

    update to trudag v2025.10.22 (#4)
    * update to trudag v2025.10.22
    
    * upgrade pip
    
    * upgrade pip in test_publication workflow
    
    * pip install requests

--- .devcontainer/S-CORE/post_create_script.sh
+++ .devcontainer/S-CORE/post_create_script.sh
@@ -5,4 +5,4 @@ source .venv/bin/activate
 
 # Install trustable
 pip install --require-hashes -r .devcontainer/S-CORE/requirements.txt
-pip install git+https://gitlab.com/CodethinkLabs/trustable/trustable@9957f12171cb898d83df5ae708fdba0a38fece2e
+pip install git+https://gitlab.com/CodethinkLabs/trustable/trustable@v2025.10.22
